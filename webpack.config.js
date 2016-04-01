777b7e2b138a98d10dac27ba55665e96e4e7bb31
lilyJi <lili.ji@colorado.edu>
2016-03-31 22:27:17 -0600

    annotations

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,57 @@
 var path = require('path');
-var webpack = require('webpack');
-
+var webpack = require('webpack');//webpack module
+//execute each module with eval, exports the config file
 module.exports = {
   devtool: 'eval',
   entry: {
     app : [
+      // create localhost server at localhost:3000
       'webpack-dev-server/client?http://localhost:3000',
-      'webpack/hot/only-dev-server',
+      'webpack/hot/only-dev-server',//allow for compnet reloaded without page reload
       './lib/index.js'],
   },
+
   output: {
-    path: path.join(__dirname, './public/js/'),
-    filename: `app.js`,
-    publicPath: '/js/'
+    path: path.join(__dirname, './public/js/'),//absolute path to output directory
+    filename: `app.js`,//the main file name is app.js
+    publicPath: '/js/' //allow index.html to access code via path /js/app.js
   },
+  //Add additional plugins to the compiler
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin()//make change while the app is running without a page reload
   ],
   node: {
-    fs: "empty"
+    fs: "empty"    // FileSystem defaulted to empty for node.
   },
+  //when refrencing 'react', replace with the path to react
   resolve: {
     alias: {
       'react': path.join(__dirname, 'node_modules', 'react')
     },
+    // Extensino should be .js, indicating 'javascript'
     extensions: ['', '.js']
   },
   resolveLoader: {
+    //if loaders can't be found, look for them in node_modules
     'fallback': path.join(__dirname, 'node_modules')
   },
-  module: {    
+  module: {
     loaders: [
     {
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      exclude: /node_modules/,
-      include: [path.join(__dirname,'./lib')]
+      test: /\.js$/,//for .js extension files
+      loaders: ['react-hot', 'babel'],// use react-hot and babel to load modules
+      exclude: /node_modules/,//don't include /node_modules
+      include: [path.join(__dirname,'./lib')]//include everything in ./lib
     },
-    {
+    {//find all xml file extensions and load as raw
       test: /\.xml$/,
       loader: "raw"
     },
-    {
+    {//find all xml file extensions and load with json-loader
       test: /\.json$/,
       loaders: ['json-loader']
     },
-    {
+    {//find all css file extensions and load with raw in root directory
       test: /\.css?$/,
       loaders: ['style', 'raw'],
       include: __dirname
